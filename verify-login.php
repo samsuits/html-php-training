03cccac5b16a9c863a696b9156b4adac5eee3c3b
Satish Samala <satish.samala@gmail.com>
2024-05-22 18:18:20 +0530

    Reset my branch

--- verify-login.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo 'You have submitted this information for login';
-
-echo "<br/>";
-echo "<br/>";
-echo 'username :  ' . $_POST['username'];
-echo "<br/>";
-echo 'password :   ' . $_POST['password'];
-?>
