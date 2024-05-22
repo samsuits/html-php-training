c11541fcea82b555812bea9cac6bcf3024c0bac6
siddu <siddarthvarmadongari@gmail.com>
2024-05-23 01:08:41 +0530

    login form

---
+++ verify.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "YOU HAVE REGISTERED SUCCESSFULLY!!";
+echo "<br/>";
+echo "<br/>";
+echo "Submitted Information:";
+echo "<br/>";
+echo "<br/>";
+echo 'Name of the user -->' . $_POST["username"];
+echo "<br/>";
+echo "<br/>";
+echo 'Password is ' . $_POST["password"];
+echo "<br/>";
+echo "<br/>";
+echo 'Depatment of the user :' . $_POST["department"];
+echo "<br/>";
+echo "<br/>";
+echo 'DOB:' . $_POST["dob"];
+echo "<br/>";
+echo "<br/>";
+echo 'contact number:' . $_POST["contact_number"];
+?>
