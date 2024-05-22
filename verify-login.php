4b3335dba6f513c03421427c7c71b0f33023c641
shivudu1601 <shivanigorantala@gmail.com>
2024-05-22 13:09:41 +0530

    php-login

--- verify-login.php
+++ verify-login.php
@@ -3,31 +3,31 @@
 
  echo "<br/>";
   
- echo Full name:".$_GET["full name"];
+ echo 'Full name:  '  .$_GET['full name'];
   
  echo "<br/>";
 
- echo Contact Number:".$_GET["contact number"];
+ echo 'Contact Number:  ' .$_GET['contact number'];
  echo "<br/>";
 
- echo Gender:".$_GET["gender"];
+ echo 'Gender:   ' .$_GET['gender'];
  echo "<br/>";
 
- echo Date Of Birth:".$_GET["date"];
+ echo 'Date Of Birth:  ' .$_GET['date'];
  echo "<br/>";
 
- echo Email:".$_GET["email"];
+ echo 'Email:  ' .$_GET['email'];
  echo "<br/>";
 
- echo Password:".$_GET["password"];
+ echo 'Password:  ' .$_GET['password'];
  echo "<br/>";
 
- echo Known Language:".$_GET["known language"];
+ echo 'Known Language:   ' .$_GET['known language'];
  echo "<br/>";
 
- echo Address:".$_GET["address"];
+ echo 'Address:  ' .$_GET['address'];
  echo "<br/>";
 
- echo Country:".$_GET["country"];
+ echo 'Country:   ' .$_GET['country'];
  echo "<br/>";
  ?>
