9a3407e163d9c68ecd8f0a4612a80ba22ff696c4
Satish Samala <satish.samala@gmail.com>
2024-05-22 18:53:25 +0530

    login page

---
+++ login-verify.php
@@ -0,0 +1,8 @@
+<?php
+
+echo '<h1>Subbmited values</h1>';
+echo 'username is : ' . $_POST['username'];
+echo "<br/>";
+echo 'password is : ' . $_POST['password'];
+echo "<br/>";
+echo 'dept. is : ' . $_POST['department'];
