c75b0b20d11d36e9bbd07e2e75c0971ce1733e78
shivudu1601 <shivanigorantala@gmail.com>
2024-05-22 13:03:51 +0530

    commit changes

---
+++ verify-login.php
@@ -0,0 +1,33 @@
+<?php
+ echo "your submitted this information for login";
+
+ echo "<br/>";
+  
+ echo Full name:".$_GET["full name"];
+  
+ echo "<br/>";
+
+ echo Contact Number:".$_GET["contact number"];
+ echo "<br/>";
+
+ echo Gender:".$_GET["gender"];
+ echo "<br/>";
+
+ echo Date Of Birth:".$_GET["date"];
+ echo "<br/>";
+
+ echo Email:".$_GET["email"];
+ echo "<br/>";
+
+ echo Password:".$_GET["password"];
+ echo "<br/>";
+
+ echo Known Language:".$_GET["known language"];
+ echo "<br/>";
+
+ echo Address:".$_GET["address"];
+ echo "<br/>";
+
+ echo Country:".$_GET["country"];
+ echo "<br/>";
+ ?>
