5599399934d248c81f5918f468f60e28153898fe
Ushasri-17 <ushasrivelishala07@gmail.com>
2024-05-22 19:06:18 +0530

    commit reg

---
+++ verify.php
@@ -0,0 +1,30 @@
+<?php
+
+
+echo '<h1>Submitted details</h1>';
+echo 'Name:' . $_POST['name'];
+echo '</br>';
+echo '</br>';
+echo 'Gender:' . $_POST['gender'];
+echo '</br>';
+echo '</br>';
+echo 'Contact:' . $_POST['Contact'];
+echo '</br>';
+echo '</br>';
+echo 'DOB:' . $_POST['date'];
+echo '</br>';
+echo '</br>';
+echo 'Email:' . $_POST['email'];
+echo '</br>';
+echo '</br>';
+echo 'City:' . $_POST['city'];
+echo '</br>';
+echo '</br>';
+echo 'Country:' . $_POST['Country'];
+echo '</br>';
+echo '</br>';
+echo 'Hobbies:' . $_POST['hobbies'];
+
+
+?>
+
