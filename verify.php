dfd7a7e4736ccca45ea2fa7b4fc86a9a39617abe
valusasindhu <valusasindhu@gmail.com>
2024-05-23 14:22:24 +0530

    accessing user details

---
+++ verify.php
@@ -0,0 +1,24 @@
+<?php
+echo "Successfully Submited";
+echo "</br>";
+echo "</br>";
+echo "Here we goo!!";
+echo "</br>";
+echo "</br>";
+ echo 'Your name is : ' . $_POST['username'];
+ echo "</br>";
+ echo "</br>";
+ echo 'email : ' . $_POST['email'];
+ echo "</br>";
+ echo "</br>";
+ echo 'Gender : ' . $_POST['gender'];
+ echo "</br>";
+ echo "</br>";
+ echo 'Education : ' . $_POST['education'];
+ echo "</br>";
+ echo "</br>";
+ echo 'location : ' . $_POST['city'];
+ echo "</br>";
+ echo "</br>";
+ 
+?>
