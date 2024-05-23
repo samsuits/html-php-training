86ba205474e8153f83d6b6c20c473803113f6b9f
Satish Samala <satish.samala@gmail.com>
2024-05-23 18:53:06 +0530

    greetings app

---
+++ greet-user.php
@@ -0,0 +1,27 @@
+<?php
+
+if (isset($_GET['name']))
+{
+    echo 'Hello ' .  $_GET['name'];
+    echo '<br/><hr/>';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greet User</title>
+</head>
+<body>
+    <h1>Please type your name</h1>
+
+    <form action="" method="get">
+        name: <input type="text" name="name">
+        <br/>
+        <br/>
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
