11860ddb999ab8ef73d6a70d27b5c7c2e6e8faf5
shivudu1601 <shivanigorantala@gmail.com>
2024-05-23 14:39:08 +0530

    commit changes

---
+++ verify.php
@@ -0,0 +1,21 @@
+<?php
+echo '<h1>your  submitted this information for registration</h1>';
+echo '<br/>';
+echo 'Full name :' .$_post['full name'];
+echo '<br/>';
+echo 'Email :' .$_post['email'];
+echo '<br/>';
+echo 'Password :' .$_post['password'];
+echo '<br/>';
+echo 'Contact number :' .$_post['contact number'];
+echo '<br/>';
+echo 'Gender:' .$_post['gender'];
+echo '<br/>';
+echo 'Date Of Birth:' .$_post['dob'];
+echo '<br/>';
+echo 'Known Languages :' .$_post['known languages'];
+echo '<br/>';
+echo 'Address :' .$_post['address'];
+echo '<br/>';
+
+?>
