1079009af67eedba85f0fea39415f10262612d6b
valusasindhu <valusasindhu@gmail.com>
2024-06-14 19:26:14 +0530

    register2

--- login-res.php
+++ login-res.php
@@ -7,22 +7,18 @@ $semester=$_POST['semester'];
 $mobileNo=$_POST['mobileno'];
 $address=$_POST['address'];
 
-$sql="INSERT INTO `students` ( `name`, `gender`, `semester`, `mobileno`, `address`) VALUES ( $name, $gender, $semester, $mobileNo, $address)";
+$sql="INSERT INTO `students` ( `name`, `gender`, `semester`, `mobileno`, `address`) VALUES ( '$name', '$gender', '$semester', '$mobileNo', '$address');";
 
-    /*<$sql = "INSERT INTO
-        login-system
-        (name, gender, semester,mobileno, address)
-        VALUES ('" . $name . "', '" . $gender. "','" . $semester . "', '" . $mobileno . "', '" . $address . "');";
+
         $result=$conn->query($sql);
 
         if($result == true)
         {
             echo 'user created';
-            echo'<br/><a href="login.html">Click here to login<a>';
 
         }
         else
         {
             echo "Error";
          }
-?>*/
+?>
