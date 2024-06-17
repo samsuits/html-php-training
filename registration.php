0117829da606a5440e1975ce42ad2062b70e1257
siddu <siddarthvarmadongari@gmail.com>
2024-06-17 19:41:24 +0530

    new

---
+++ registration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registration</title>
+</head>
+<body>
+    <h1>Registration</h1>
+     <form method = "POST">
+        Name: <input type="text" name="name"></br></br>
+        Email: <input type="email" name = "email"></br></br>
+        Mobile: <input type="number" name = "mobile"></br></br>
+        Address: <textarea name="address" ></textarea></br></br>
+        Password: <input type="password" name = "password"></br></br>
+        <input type="submit" name ="submit"></br></br>
+     ></form>
+</body>
+</html>
