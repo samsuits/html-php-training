0155a616b9709155c358adebf932fa8e3bd3e9c0
Ramyasreekasula07 <ramyasree.kasula1628@gmail.com>
2024-06-17 18:56:59 +0530

    reset for db connection

---
+++ db-connection.php
@@ -0,0 +1,15 @@
+<?php
+$hostname='localhost';
+$user     = 'root';
+$password = '';
+
+$conn = mysqli_connect($hostname, $user, $password);
+
+if ($conn)
+{
+    echo 'DB Connection Successful';
+}
+else
+{
+    echo 'DB Connectio failed';
+}
